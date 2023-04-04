7f566ddd0a293b05016a34747cee211aa97bac8a
Cjessiel <62605235+Cjessiel@users.noreply.github.com>
2023-04-04 15:08:59 -0300

    Desenvolvimento de models

---
+++ src/main/java/com/api/attornatus/model/Endereco.java
@@ -0,0 +1,33 @@
+package com.api.attornatus.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Enderecos")
+@Table(name = "enderecos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "pessoa_id")
+    private Pessoa pessoa;
+    private Boolean primario;
+    @Column(length = 100)
+    private String logradouro;
+    @Column(length = 8)
+    private String cep;
+    @Column(length = 10)
+    private String numero;
+    @Column(length = 150)
+    private String cidade;
+
+}
