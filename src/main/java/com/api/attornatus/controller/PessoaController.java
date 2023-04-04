a1a167389192aab2d9b7edd0caec29b501861770
Cjessiel <62605235+Cjessiel@users.noreply.github.com>
2023-04-04 15:12:27 -0300

    Inicio de desenvolvimento de classe controller

---
+++ src/main/java/com/api/attornatus/controller/PessoaController.java
@@ -0,0 +1,11 @@
+package com.api.attornatus.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pessoa")
+public class PessoaController {
+
+
+}
