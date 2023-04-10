2331f4094be1c811bad95bda2fa3f91f0cb55629
Cjessiel <62605235+Cjessiel@users.noreply.github.com>
2023-04-10 10:04:09 -0300

    Desenvolvimento de primeiro cenario de testes para cadastro de endereço

---
+++ src/test/java/com/api/attornatus/controller/EnderecoControllerTest.java
@@ -0,0 +1,30 @@
+package com.api.attornatus.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class EnderecoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("Deveria devolver codigo 405 quando ID não informado no path")
+    void cadastrarNovoEndereco_cenario1() throws Exception {
+        var response = mvc.perform(post("/endereco"))
+                .andReturn()
+                .getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.METHOD_NOT_ALLOWED.value());
+    }
+}
